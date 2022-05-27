b64f1abfadea47890caae093e6b5e473c8ddc0fc
Biro <Leonardo09012003@gmail.com>
2022-05-27 19:54:32 +0200

    🎊 Fixed some bugs
    Bugs related to web service responses

--- src/main/java/com/example/solarsystem_webservice/RequestHandler.java
+++ src/main/java/com/example/solarsystem_webservice/RequestHandler.java
@@ -1,3 +1,6 @@
+
+//https://api.le-systeme-solaire.net/en/
+
 package com.example.solarsystem_webservice;
 
 import org.json.JSONObject;
@@ -11,10 +14,16 @@ import java.net.URLEncoder;
 public class RequestHandler {
 
     // Open data powered by 'Le Système solaire à portée de votre souris' --> https://api.le-systeme-solaire.net/en/
-    private static final String webService_URL = "https://api.le-systeme-solaire.net/rest/bodies?";
 
-    public static String getInfo(String parameters) {
-        URL server;
+
+    //first get
+    public static final String idbody_URL = "https://api.le-systeme-solaire.net/rest/bodies/"; //+ idbody
+
+    //second get
+    public static final String bodyType_URL = "https://api.le-systeme-solaire.net/rest.php/bodies?filter[]=bodyType,eq,";
+
+    public static String getInfo(URL urlRequest) {
+
         HttpsURLConnection service;
         String xmlResponse = null;
 
@@ -22,12 +31,8 @@ public class RequestHandler {
         int status;
 
         try {
-
-            // create the request URL towards open data web service encoding "idBody" value according UTF-8 standard
-            server = new URL(webService_URL + URLEncoder.encode(parameters, "UTF-8"));
-
             //set up connection to web service
-            service = (HttpsURLConnection) server.openConnection();
+            service = (HttpsURLConnection) urlRequest.openConnection();
 
             //set property of the request
             //require header setting related to interrogated host
@@ -58,16 +63,68 @@ public class RequestHandler {
 
             //create xml string from json response
             BufferedReader input = new BufferedReader(new InputStreamReader(service.getInputStream(), "UTF-8"));
-            StringBuilder jsonString = new StringBuilder();
+            StringBuffer jsonString = new StringBuffer();
             String line;
-            while ((line = input.readLine()) != null) {
-                jsonString.append(line);
+
+            if ((line = input.readLine()) != null) {
+                JSONObject json = new JSONObject(line);
+                xmlResponse = XML.toString(json);
             }
 
-            JSONObject json = new JSONObject(jsonString);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-            xmlResponse = XML.toString(json);
+        return xmlResponse;
+    }
 
+
+
+    public static String getBodyType(URL urlRequest) {
+
+        HttpsURLConnection service;
+        String xmlResponse = null;
+
+        //stato della richiesta ( es 200, 404 ecc.)
+        int status;
+
+        try {
+            //set up connection to web service
+            service = (HttpsURLConnection) urlRequest.openConnection();
+
+            //set property of the request
+            //require header setting related to interrogated host
+            service.setRequestProperty("Host", "le-systeme-solaire.net");
+
+            //require header setting related to response format (JSON)
+            service.setRequestProperty("Accept", "application/json");
+
+            //required header setting related to response encoding (UTF-8)
+            service.setRequestProperty("Accept-Charset", "UTF-8");
+
+            //set request method to 'GET'
+            service.setRequestMethod("GET");
+
+            //receive activation
+            service.setDoInput(true);
+
+            //establish the connection
+            service.connect();
+
+            status = service.getResponseCode();
+
+            //check the status code
+            if (status != 200) {
+                //error
+                return null;
+            }
+
+            String line;
+            BufferedReader input = new BufferedReader(new InputStreamReader(service.getInputStream(), "UTF-8"));
+            if ((line = input.readLine()) != null) {
+                JSONObject json = new JSONObject(line);
+                xmlResponse = XML.toString(json,"bodyType"); //establish the root
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
--- src/main/java/com/example/solarsystem_webservice/SolarSystem.java
+++ src/main/java/com/example/solarsystem_webservice/SolarSystem.java
@@ -1,19 +1,30 @@
 //https://api.le-systeme-solaire.net/en/
 
 package com.example.solarsystem_webservice;
+
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.net.URL;
+import java.net.URLEncoder;
 import java.util.concurrent.TimeUnit;
 
 @WebServlet(name = "SolarSystem", value = "/*")
 public class SolarSystem extends HttpServlet {
 
+    // TODO: 27/05/2022 fix unordered response xml tags
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("text/html");
-
+        URL urlRequest;
         // save URL request
         String url = request.getRequestURL().toString().toLowerCase();
 
@@ -33,34 +44,37 @@ public class SolarSystem extends HttpServlet {
                     return;
                 }
 
+                // create the request URL towards open data web service encoding "idBody" value according UTF-8 standard
+                urlRequest = new URL(RequestHandler.idbody_URL + URLEncoder.encode(idBody, "UTF-8"));
+
                 //request data about body
                 //check for some issue during comunication with open data web service
-                String xmlResponse =RequestHandler.getInfo(idBody);
-               if (xmlResponse==null){
+                String xmlResponse = RequestHandler.getInfo(urlRequest);
+                if (xmlResponse==null){
 
-                   //try another request then 1 second
-                   TimeUnit.SECONDS.sleep(1);
+                    //try another request then 1 second
+                    TimeUnit.SECONDS.sleep(1);
 
-                   if ((xmlResponse=RequestHandler.getInfo(idBody))==null){
-                       // send error
-                       response.sendError(400, "No connection towards to dataset");
-                       return;
-                   }
-
-               }
+                    if ((xmlResponse=RequestHandler.getInfo(urlRequest))==null){
+                        // send error
+                        response.sendError(400, "No connection towards to dataset");
+                        return ;
+                    }
+                }
+                xmlResponse = "<body>" + xmlResponse + "</body>";
 
                 response.setStatus(200);
-
                 PrintWriter printResponse = response.getWriter();
                 printResponse.write(xmlResponse);
                 printResponse.flush();
                 printResponse.close();
 
 
-            }catch (InterruptedException e) {
+            } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-        }else if(url.endsWith("list")){
+
+        } else if (url.endsWith("list")) {
             try {
 
                 //save body id contained in URL
@@ -75,35 +89,38 @@ public class SolarSystem extends HttpServlet {
                     return;
                 }
 
+                // create the request URL towards open data web service encoding "idBody" value according UTF-8 standard
+                urlRequest = new URL(RequestHandler.bodyType_URL + URLEncoder.encode(bodyType, "UTF-8"));
+
                 //request data about body
                 //check for some issue during comunication with open data web service
-                String xmlResponse =RequestHandler.getInfo("filter[]=bodyType,eq,"+bodyType);
+
+                String xmlResponse = RequestHandler.getBodyType(urlRequest);
                 if (xmlResponse==null){
 
                     //try another request then 1 second
                     TimeUnit.SECONDS.sleep(1);
 
-                    if ((xmlResponse=RequestHandler.getInfo("filter[]=bodyType,eq,"+bodyType))==null){
+                    if ((xmlResponse=RequestHandler.getBodyType(urlRequest))==null){
                         // send error
                         response.sendError(400, "No connection towards to dataset");
-                        return;
+                        return ;
                     }
-
                 }
 
                 response.setStatus(200);
-
                 PrintWriter printResponse = response.getWriter();
                 printResponse.write(xmlResponse);
                 printResponse.flush();
                 printResponse.close();
 
 
-            }catch (InterruptedException e) {
+            } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-        }else {
+        } else {
             response.sendError(400, "Bad request");
         }
     }
+
 }
